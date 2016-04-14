d61bc09083b35825d8d92bed0ad1ea673730eced
thisisazk <thisisazk@gmail.com>
2016-04-14 15:21:54 +0500

    Added form validation

--- script.js
+++ script.js
@@ -6,3 +6,27 @@ function emailChk() {
 		return false;
 	}
 }
+
+function contChk() {
+	var x = document.getElementById('ctxtName').value;
+	var y = document.getElementById('ctxtEmail').value;
+	var z = document.getElementById('ctxtMessage').value;
+
+	if (x == "") {
+		alert("Name cannot be empty!!");
+		document.getElementById('ctxtName').focus();
+		return false;
+	} 
+	else if (y == "") {
+		alert("Email cannot be empty");
+		document.getElementById('ctxtEmail').focus();
+		return false;
+	}
+	else if (z == "") {
+		alert("Please don't send us empty emails");
+		document.getElementById('ctxtMessage').focus();
+		return false;
+	}
+
+	alert("Your message is recieved. We will contact you soon.");
+}
